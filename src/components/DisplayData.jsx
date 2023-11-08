1900da534c215b81377b81ecd7523ddf8a4d3e9a
Amir Sheikhloo <amirhossein.sheikhloo@gmailcom>
2023-11-08 14:02:02 +0330

    Adding Sort Button for Country Cell

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -7,6 +7,7 @@ import {
   TableHead,
   TableRow,
   TablePagination,
+  Button,
 } from "@mui/material";
 import { multiStepContext } from "./StepContext";
 
@@ -14,8 +15,7 @@ export default function DisplayData() {
   const { finalData } = useContext(multiStepContext);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [filterValue, setFilterValue] = useState("");
-  const [filteredData, setFilteredData] = useState([]);
+  const [sortDirection, setSortDirection] = useState("asc");
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -24,44 +24,44 @@ export default function DisplayData() {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
-    setFilterValue(""); // Reset the filter value
   };
 
-  const handleChangeFilter = (event) => {
-    const value = event.target.value;
-    setFilterValue(value);
-
-    // Filter the data based on the filter value
-    const filteredData = finalData.filter((data) =>
-      data.country.toLowerCase().includes(value.toLowerCase())
-    );
-    setFilteredData(filteredData);
+  const handleSort = () => {
+    const direction = sortDirection === "asc" ? "desc" : "asc";
+    setSortDirection(direction);
   };
+
+  const sortedData = [...finalData].sort((a, b) => {
+    const nameA = a.country.toLowerCase();
+    const nameB = b.country.toLowerCase();
+    if (nameA < nameB) return sortDirection === "asc" ? -1 : 1;
+    if (nameA > nameB) return sortDirection === "asc" ? 1 : -1;
+    return 0;
+  });
+
+  const paginatedData = sortedData.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
 
   return (
     <div>
-      <input
-        type="text"
-        value={filterValue}
-        onChange={handleChangeFilter}
-        placeholder="Filter by country"
-      />
       <TableContainer style={{ display: "flex", justifyContent: "center" }}>
-        <Table
-          border="1"
-          style={{ width: "70%", justifyContent: "center" }}
-          size="small"
-          aria-label="captain table"
-        >
+        <Table border="1" style={{ width: "70%", justifyContent: "center" }} size="small" aria-label="captain table">
           <TableHead>
-            <TableRow
-              style={{ backgroundColor: "burlywood", color: "aliceblue" }}
-            >
+            <TableRow style={{ backgroundColor: "burlywood", color: "aliceblue" }}>
               <TableCell>First Name</TableCell>
               <TableCell>Last Name</TableCell>
               <TableCell>Contact Number</TableCell>
               <TableCell>Email Address</TableCell>
-              <TableCell>Country</TableCell>
+              <TableCell>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                  <span>Country</span>
+                  <Button size="small" variant="contained" onClick={handleSort}>
+                    Sort
+                  </Button>
+                </div>
+              </TableCell>
               <TableCell>District</TableCell>
               <TableCell>City</TableCell>
               <TableCell>Landmark</TableCell>
@@ -69,28 +69,26 @@ export default function DisplayData() {
             </TableRow>
           </TableHead>
           <TableBody>
-            {(filterValue === "" ? finalData : filteredData)
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((data) => (
-                <TableRow key={data.email}>
-                  <TableCell>{data.firstname}</TableCell>
-                  <TableCell>{data.lastname}</TableCell>
-                  <TableCell>{data.contact}</TableCell>
-                  <TableCell>{data.email}</TableCell>
-                  <TableCell>{data.country}</TableCell>
-                  <TableCell>{data.district}</TableCell>
-                  <TableCell>{data.city}</TableCell>
-                  <TableCell>{data.landmark}</TableCell>
-                  <TableCell>{data.postcode}</TableCell>
-                </TableRow>
-              ))}
+            {paginatedData.map((data) => (
+              <TableRow key={data.email}>
+                <TableCell>{data.firstname}</TableCell>
+                <TableCell>{data.lastname}</TableCell>
+                <TableCell>{data.contact}</TableCell>
+                <TableCell>{data.email}</TableCell>
+                <TableCell>{data.country}</TableCell>
+                <TableCell>{data.district}</TableCell>
+                <TableCell>{data.city}</TableCell>
+                <TableCell>{data.landmark}</TableCell>
+                <TableCell>{data.postcode}</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
       <TablePagination
         rowsPerPageOptions={[5, 10, 25]}
         component="div"
-        count={filterValue === "" ? finalData.length : filteredData.length}
+        count={sortedData.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onPageChange={handleChangePage}
