623432e8c8502fea15116d04dbc3898fac566cf4
Amir Sheikhloo <amirhossein.sheikhloo@gmailcom>
2023-11-12 13:26:29 +0330

    Adding pagination by 5 10 15

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -1,98 +1,60 @@
-import React, { useContext, useState } from "react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  TablePagination,
-  Button,
-} from "@mui/material";
+import * as React from "react";
+import { DataGrid, GridPagination } from "@mui/x-data-grid";
 import { multiStepContext } from "./StepContext";
 
 export default function DisplayData() {
-  const { finalData } = useContext(multiStepContext);
-  const [page, setPage] = useState(0);
-  const [rowsPerPage, setRowsPerPage] = useState(5);
-  const [sortDirection, setSortDirection] = useState("asc");
+  const { finalData } = React.useContext(multiStepContext);
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
+  // Prepare data for DataGrid
+  const columns = [
+    { field: "firstname", headerName: "First Name", width: 150 },
+    { field: "lastname", headerName: "Last Name", width: 150 },
+    { field: "contact", headerName: "Contact Number", width: 180 },
+    { field: "email", headerName: "Email Address", width: 240 },
+    { field: "country", headerName: "Country", width: 150 },
+    { field: "district", headerName: "District", width: 150 },
+    { field: "city", headerName: "City", width: 150 },
+    { field: "landmark", headerName: "Landmark", width: 180 },
+    { field: "postcode", headerName: "Postal Code", width: 150 },
+  ];
+
+  const rows = finalData.map((data, index) => ({
+    id: index + 1,
+    ...data,
+  }));
+
+  // State for page and pageSize
+  const [page, setPage] = React.useState(0);
+  const [pageSize, setPageSize] = React.useState(5);
+
+  const handleChangePage = (params) => {
+    setPage(params.page);
   };
 
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
+  const handleChangePageSize = (params) => {
+    setPageSize(params.pageSize);
+    setPage(0); // Reset page to the first page when changing the pageSize
   };
-
-  const handleSort = () => {
-    const direction = sortDirection === "asc" ? "desc" : "asc";
-    setSortDirection(direction);
-  };
-
-  const sortedData = [...finalData].sort((a, b) => {
-    const nameA = a.country.toLowerCase();
-    const nameB = b.country.toLowerCase();
-    if (nameA < nameB) return sortDirection === "asc" ? -1 : 1;
-    if (nameA > nameB) return sortDirection === "asc" ? 1 : -1;
-    return 0;
-  });
-
-  const paginatedData = sortedData.slice(
-    page * rowsPerPage,
-    page * rowsPerPage + rowsPerPage
-  );
 
   return (
-    <div>
-      <TableContainer style={{ display: "flex", justifyContent: "center" }}>
-        <Table border="1" style={{ width: "70%", justifyContent: "center" }} size="small" aria-label="captain table">
-          <TableHead>
-            <TableRow style={{ backgroundColor: "burlywood", color: "aliceblue" }}>
-              <TableCell>First Name</TableCell>
-              <TableCell>Last Name</TableCell>
-              <TableCell>Contact Number</TableCell>
-              <TableCell>Email Address</TableCell>
-              <TableCell>
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                  <span>Country</span>
-                  <Button size="small" variant="contained" onClick={handleSort}>
-                    Sort
-                  </Button>
-                </div>
-              </TableCell>
-              <TableCell>District</TableCell>
-              <TableCell>City</TableCell>
-              <TableCell>Landmark</TableCell>
-              <TableCell>Postal Code</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {paginatedData.map((data) => (
-              <TableRow key={data.email}>
-                <TableCell>{data.firstname}</TableCell>
-                <TableCell>{data.lastname}</TableCell>
-                <TableCell>{data.contact}</TableCell>
-                <TableCell>{data.email}</TableCell>
-                <TableCell>{data.country}</TableCell>
-                <TableCell>{data.district}</TableCell>
-                <TableCell>{data.city}</TableCell>
-                <TableCell>{data.landmark}</TableCell>
-                <TableCell>{data.postcode}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[5, 10, 25]}
-        component="div"
-        count={sortedData.length}
-        rowsPerPage={rowsPerPage}
+    <div style={{ height: 400, width: "100%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        checkboxSelection
         page={page}
+        pageSize={pageSize}
         onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
+        onPageSizeChange={handleChangePageSize}
+        rowCount={rows.length}
+        components={{
+          pagination: (props) => (
+            <GridPagination
+              {...props}
+              rowsPerPageOptions={[5, 10, 15]} // Set the available rows per page options
+            />
+          ),
+        }}
       />
     </div>
   );
