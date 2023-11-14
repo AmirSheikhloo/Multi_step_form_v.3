94dc6f66396bca559596b3dd4beae52a850a8162
Amir Sheikhloo <amirhossein.sheikhloo@gmailcom>
2023-11-14 12:23:14 +0330

    Adding a Delete/Cansel button for every row

--- src/components/DisplayData.jsx
+++ src/components/DisplayData.jsx
@@ -1,21 +1,91 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { DataGrid, GridPagination } from "@mui/x-data-grid";
 import { multiStepContext } from "./StepContext";
 
 export default function DisplayData() {
-  const { finalData } = React.useContext(multiStepContext);
+  const { finalData, setFinalData } = React.useContext(multiStepContext); // Define setFinalData
+  const [selectedRow, setSelectedRow] = React.useState(null);
 
+  
   // Prepare data for DataGrid
   const columns = [
-    { field: "firstname", headerName: "First Name", width: 150 },
-    { field: "lastname", headerName: "Last Name", width: 150 },
-    { field: "contact", headerName: "Contact Number", width: 180 },
-    { field: "email", headerName: "Email Address", width: 240 },
-    { field: "country", headerName: "Country", width: 150 },
-    { field: "district", headerName: "District", width: 150 },
-    { field: "city", headerName: "City", width: 150 },
-    { field: "landmark", headerName: "Landmark", width: 180 },
-    { field: "postcode", headerName: "Postal Code", width: 150 },
+    {
+      field: "firstname",
+      headerName: "First Name",
+      width: 130,
+      disableColumnMenu: true,
+    },
+    {
+      field: "lastname",
+      headerName: "Last Name",
+      width: 130,
+      disableColumnMenu: true,
+    },
+    {
+      field: "contact",
+      headerName: "Contact Number",
+      width: 170,
+      disableColumnMenu: true,
+    },
+    {
+      field: "email",
+      headerName: "Email Address",
+      width: 210,
+      disableColumnMenu: true,
+    },
+    {
+      field: "country",
+      headerName: "Country",
+      width: 120,
+      disableColumnMenu: true,
+    },
+    {
+      field: "district",
+      headerName: "District",
+      width: 120,
+      disableColumnMenu: true,
+    },
+    { field: "city", headerName: "City", width: 120, disableColumnMenu: true },
+    {
+      field: "landmark",
+      headerName: "Landmark",
+      width: 160,
+      disableColumnMenu: true,
+    },
+    {
+      field: "postcode",
+      headerName: "Postal Code",
+      width: 130,
+      disableColumnMenu: true,
+    },
+    {
+      field: "action",
+      headerName: "Action",
+      width: 140,
+      renderCell: (params) => (
+        <div>
+          {selectedRow === params.id ? (
+            <>
+              <button
+                className="action-button"
+                onClick={() => handleDelete(params.id)}
+              >
+                Delete
+              </button>
+              <span> </span>
+              <button
+                className="action-button"
+                onClick={() => setSelectedRow(null)}
+              >
+                Cancel
+              </button>
+            </>
+          ) : (
+            <button onClick={() => setSelectedRow(params.id)}>More</button>
+          )}
+        </div>
+      ),
+    },
   ];
 
   const rows = finalData.map((data, index) => ({
@@ -35,9 +105,13 @@ export default function DisplayData() {
     setPageSize(params.pageSize);
     setPage(0); // Reset page to the first page when changing the pageSize
   };
+  const handleDelete = (id) => {
+    const updatedRows = rows.filter((row) => row.id !== id);
+    setFinalData(updatedRows); // Use setFinalData to update the state
+  };
 
   return (
-    <div style={{ height: 400, width: "100%" }}>
+    <div style={{ height: 400, width: "98%" }}>
       <DataGrid
         rows={rows}
         columns={columns}
